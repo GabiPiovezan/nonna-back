15edaf50171170b63adb5f3eac8d4149b470f1bf
gabriella62318596 <gabymarqueserosa@gmail.com>
2026-07-29 21:46:14 -0300

    primeiro commit

--- nonna-back/src/main/java/br/com/nonna_back/controllers/HealthCheckController.java
+++ nonna-back/src/main/java/br/com/nonna_back/controllers/HealthCheckController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HealthCheckController {
+public class HealthCheckController  {
     @GetMapping("health-check/liveness")
     public String liveness(){
         return "OK";
